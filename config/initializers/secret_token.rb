4474bf12733bdafacdb0d2497c80233b02ccbd0d
Nazarchuk <gitnazarchuk@gmail.com>
2015-02-15 01:28:30 +0300

    cart redisign

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Shop::Application.config.secret_key_base = '987a65f2d8ebb9c3c9c289949b71d2a4f0e5ca556940e88e88fe05c13a0097b7de8c31d58f3ad07ba5132c8eaefefbf62d7e4d6c491c0f754a757e465e55308a'
