ccc74f55ef0a2e51bba0edcdf7b9c1f72b8e7228
Nazarchuk <gitnazzarchuk@gmail.com>
2015-11-09 16:35:25 +0300

    add gitignore

--- db/migrate/20150406232829_create_prices.rb
+++ db/migrate/20150406232829_create_prices.rb
@@ -3,7 +3,6 @@ class CreatePrices < ActiveRecord::Migration
     create_table :prices do |t|
       t.integer :price
       t.belongs_to :size, index: true
-      t.belongs_to :mattress, index: true
       t.timestamps
     end
   end
